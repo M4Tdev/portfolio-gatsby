ac2a8b633e0b546ce757500ae2cbf00e3deb71de
M4T <mateo.lesiuk@gmail.com>
2019-06-09 16:06:04 +0200

    Install and configure gatsby transformer remark plugin, pull about text from markdown file

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { graphql, useStaticQuery } from 'gatsby';
 
 import Shape from '../assets/Vector1.svg';
 import SectionName from './UI/SectionName';
@@ -12,35 +13,43 @@ const StyledAbout = styled.div`
   color: black;
   z-index: 1;
   box-sizing: border-box;
+  overflow: hidden;
 `;
 
 const StyledShape = styled(Shape)`
   position: absolute;
   left: 0;
   bottom: 0;
+  margin-left: -6rem;
   z-index: -1;
 `;
 
 const StyledContent = styled.p`
-  font-size: 1.6rem;
+  font-size: 1.5rem;
   font-weight: var(--regular);
-  margin: 3rem 1rem 0 11rem;
+  margin: 3rem 1rem 0 7rem;
 `;
 
-const About = () => (
-  <StyledAbout>
-    <StyledShape />
-    <SectionName width="15rem" height="5rem">
-      About me
-    </SectionName>
-    <StyledContent>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et sem eu augue tristique pulvinar quis vitae
-      mauris. Nulla varius mi vel nisi congue blandit. Etiam viverra tellus nec maximus rhoncus. Phasellus metus mauris,
-      vestibulum eget mi quis, convallis mattis quam. Cras eros ante, condimentum fringilla metus ac, commodo imperdiet
-      eros. Duis ut interdum tellus, sed blandit mauris. Donec laoreet congue mi, vitae scelerisque mauris dictum sit
-      amet. Praesent congue scelerisque efficitur.
-    </StyledContent>
-  </StyledAbout>
-);
+const About = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "about.md" }) {
+        childMarkdownRemark {
+          html
+        }
+      }
+    }
+  `);
+
+  return (
+    <StyledAbout>
+      <StyledShape />
+      <SectionName width="12rem" height="4rem" fontsize="1.6rem" margin="2rem 0 0 2rem" color="black">
+        About me
+      </SectionName>
+      <StyledContent dangerouslySetInnerHTML={{ __html: data.file.childMarkdownRemark.html }} />
+    </StyledAbout>
+  );
+};
 
 export default About;
