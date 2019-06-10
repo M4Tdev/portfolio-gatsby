a54ad4aaa9b0460a5553f84418bcb6dfe1a881d8
M4T <mateo.lesiuk@gmail.com>
2019-06-10 13:04:15 +0200

    Install and configure gatsby external link remark plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,6 +43,19 @@ module.exports = {
         path: `${__dirname}/src/content`,
       },
     },
-    { resolve: 'gatsby-transformer-remark' },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-external-links',
+            options: {
+              target: '_self',
+              rel: 'nofollow',
+            },
+          },
+        ],
+      },
+    },
   ],
 };
