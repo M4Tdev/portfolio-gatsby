0f955715b6076d9151bc6aa24b1550394599677d
M4T <mateo.lesiuk@gmail.com>
2019-06-11 14:37:56 +0200

    Fix gradient transition

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -9,32 +9,13 @@ const StyledPortfolio = styled.div`
   position: relative;
   width: 100vw;
   height: auto;
-  background: linear-gradient(150.64deg, #119cf5 0%, #2d74e5 15%);
+  background: linear-gradient(150.64deg, #119cf5 0%, #2d74e5 27%);
   z-index: 2;
 `;
 
 const PortfolioSection = styled.div`
   width: 90%;
   margin: 0 auto;
-`;
-
-const PortfolioSectionTitle = styled.h1`
-  text-align: center;
-  margin: 4rem auto 0.1rem;
-  font-size: 2rem;
-  font-weight: var(--bold);
-`;
-
-const StyledLine = styled.hr`
-  width: ${props => props.width};
-  background-color: white;
-  margin: 0 auto 1rem;
-`;
-
-const PortfolioSectionContent = styled.p`
-  text-align: center;
-  font-size: 1.5rem;
-  font-weight: var(--medium);
 `;
 
 const PortfolioSectionProjects = styled.div`
@@ -75,25 +56,6 @@ const Portfolio = () => {
         Portfolio
       </SectionName>
       <PortfolioSection>
-        <PortfolioSectionTitle>Skills</PortfolioSectionTitle>
-        <StyledLine width="8rem" />
-        <PortfolioSectionContent>HTML - CSS - SASS - JAVASCRIPT</PortfolioSectionContent>
-      </PortfolioSection>
-      <PortfolioSection>
-        <PortfolioSectionTitle>Technologies</PortfolioSectionTitle>
-        <StyledLine width="16rem" />
-        <PortfolioSectionContent>
-          REACT - REDUX - NODEJS - EXPRESS - MONGODB - STYLED COMPONENTS - GRAPHQL - GATSBY
-        </PortfolioSectionContent>
-      </PortfolioSection>
-      <PortfolioSection>
-        <PortfolioSectionTitle>Tools</PortfolioSectionTitle>
-        <StyledLine width="8rem" />
-        <PortfolioSectionContent>GIT - NPM - WEBPACK - BABEL - FIREBASE</PortfolioSectionContent>
-      </PortfolioSection>
-      <PortfolioSection>
-        <PortfolioSectionTitle>Projects</PortfolioSectionTitle>
-        <StyledLine width="12rem" />
         <PortfolioSectionProjects>
           {data.allMarkdownRemark.edges.map(project => (
             <PortfolioProject key={project.node.frontmatter.title} project={project} />
