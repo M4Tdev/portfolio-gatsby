25f4a3dad48d8fae3be61352bc5dac31cfc67e8d
M4T <mateo.lesiuk@gmail.com>
2019-06-12 22:08:25 +0200

    Install gatsby plugin netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
     description: 'Portfolio of Mateusz Lesiuk the Front-End Developer',
   },
   plugins: [
+    'gatsby-plugin-netlify',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
     {
