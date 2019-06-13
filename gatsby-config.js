a4698a0ed18fec3cadf94052be5ab42840374116
M4T <mateo.lesiuk@gmail.com>
2019-06-13 14:51:52 +0200

    Changed link target to blank

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,7 +53,7 @@ module.exports = {
           {
             resolve: 'gatsby-remark-external-links',
             options: {
-              target: '_self',
+              target: '_blank',
               rel: 'nofollow',
             },
           },
