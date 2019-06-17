fb5f8d70b1a7537bba7d988bf6ed18d42fe0eb62
M4T <mateo.lesiuk@gmail.com>
2019-06-17 08:45:01 +0200

    Add all media queries, some styles changes

--- src/components/UI/GoToTopButton.js
+++ src/components/UI/GoToTopButton.js
@@ -5,19 +5,64 @@ import { ChevronUp } from 'styled-icons/fa-solid';
 const GoToTop = styled.button`
   position: fixed;
   bottom: 3rem;
-  right: 3rem;
-  background-color: white;
+  right: 2rem;
+  background-color: var(--primaryLight);
   border: none;
-  padding: 2rem;
+  /* padding: 2rem; */
+  height: 5rem;
+  width: 5rem;
   border-radius: 50%;
   z-index: 2;
-  box-shadow: 0.2rem 0.2rem 3rem var(--customBlack);
+  box-shadow: 0.3rem 0.3rem 1rem rgba(0, 0, 0, 0.3);
   display: ${props => (props.show ? 'block' : 'none')};
+  opacity: 0.75;
+  cursor: pointer;
+
+  @media ${({ theme }) => theme.mediaQueries.smallest} {
+    height: 5.5rem;
+    width: 5.5rem;
+    bottom: 3.5rem;
+    right: 2.5rem;
+  }
+
+  @media ${({ theme }) => theme.mediaQueries.small} {
+    bottom: 4rem;
+    right: 3rem;
+  }
+
+  @media ${({ theme }) => theme.mediaQueries.smallMedium} {
+    height: 6.5rem;
+    width: 6.5rem;
+    bottom: 5rem;
+    right: 4rem;
+  }
+
+  @media ${({ theme }) => theme.mediaQueries.medium} {
+    height: 8rem;
+    width: 8rem;
+    bottom: 8rem;
+    right: 6rem;
+  }
+
+  @media ${({ theme }) => theme.mediaQueries.large} {
+    height: 6rem;
+    width: 6rem;
+    bottom: 6rem;
+    right: 5rem;
+  }
+
+  @media ${({ theme }) => theme.mediaQueries.largest} {
+    height: 5.5rem;
+    width: 5.5rem;
+    bottom: 6rem;
+    right: 6rem;
+  }
 `;
 
 const StyledChevron = styled(ChevronUp)`
-  min-width: 1rem;
-  color: var(--customBlack);
+  width: 1.5rem;
+  color: white;
+  fill: currentColor;
 `;
 
 const GoToTopButton = () => {
@@ -25,7 +70,6 @@ const GoToTopButton = () => {
 
   const displayButton = () => {
     if (window.pageYOffset <= 100) {
-      console.log(window.pageYOffset > 0);
       setShowToTop(window.pageYOffset > 0);
     }
   };
