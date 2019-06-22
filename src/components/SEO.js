5943aba47704e0260bbab6a0151b37cf9d9f26d0
M4T <mateo.lesiuk@gmail.com>
2019-06-22 18:42:08 +0200

    Move static query with graphql to SEO component, where it is used

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,19 +1,35 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 import Helmet from 'react-helmet';
 
-const SEO = ({ data }) => (
-  <Helmet>
-    <title>{data.site.siteMetadata.title}</title>
-    <meta charset="UTF-8" />
-    <meta name="author" content={data.site.siteMetadata.author} />
-    <meta name="description" content={data.site.siteMetadata.description} />
-    <meta property="og:title" content={data.site.siteMetadata.title} />
-    <meta property="og:type" content="website" />
-    <meta property="og:site_name" content={data.site.siteMetadata.title} />
-    <meta property="og:description" content={data.site.siteMetadata.description} />
-    <meta property="og:image:secure_url" content={`${data.site.siteMetadata.link}/favicon.ico`} />
-    <meta property="og:url" content={data.site.siteMetadata.link} />
-  </Helmet>
-);
+const SEO = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          author
+          description
+          link
+        }
+      }
+    }
+  `);
+
+  return (
+    <Helmet>
+      <title>{data.site.siteMetadata.title}</title>
+      <meta charset="UTF-8" />
+      <meta name="author" content={data.site.siteMetadata.author} />
+      <meta name="description" content={data.site.siteMetadata.description} />
+      <meta property="og:title" content={data.site.siteMetadata.title} />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content={data.site.siteMetadata.title} />
+      <meta property="og:description" content={data.site.siteMetadata.description} />
+      <meta property="og:image:secure_url" content={`${data.site.siteMetadata.link}/favicon.ico`} />
+      <meta property="og:url" content={data.site.siteMetadata.link} />
+    </Helmet>
+  );
+};
 
 export default SEO;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { graphql, useStaticQuery } from 'gatsby';
 
 import Layout from '../components/UI/Layout';
 import SEO from '../components/SEO';
@@ -9,31 +8,16 @@ import About from '../components/About';
 import Portfolio from '../components/Portfolio';
 import Contact from '../components/Contact';
 
-const IndexPage = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      site {
-        siteMetadata {
-          title
-          author
-          description
-          link
-        }
-      }
-    }
-  `);
-
-  return (
-    <Layout>
-      <>
-        <SEO data={data} />
-        <Hero />
-        <About />
-        <Portfolio />
-        <Contact />
-      </>
-    </Layout>
-  );
-};
+const IndexPage = () => (
+  <Layout>
+    <>
+      <SEO />
+      <Hero />
+      <About />
+      <Portfolio />
+      <Contact />
+    </>
+  </Layout>
+);
 
 export default IndexPage;
